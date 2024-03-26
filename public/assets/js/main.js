fa1e89eb9db445285ec2ed399301ea66748b6f10
ZhenL2023 <146369682+ZhenL2023@users.noreply.github.com>
2024-03-26 13:32:10 -0400

    Delete public directory

--- public/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// Select the hamburger icon and the menu
-const burgerIcon = document.querySelector('.burger-icon');
-const navMenu = document.querySelector('.menu');
-
-// Listen for click events on the burger icon
-burgerIcon.addEventListener('click', function() {
-    // Toggle the 'active' class on the menu
-    navMenu.classList.toggle('active');
-});
